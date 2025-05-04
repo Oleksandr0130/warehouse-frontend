19b5316611fc54dcbcf437bff59a54c4f9870dac
Oleksandr0130 <olek1995sta@gmail.com>
2025-05-04 20:02:20 +0200

    new update remowe from Warehouse Item

--- src/components/Confirmation.tsx
+++ src/components/Confirmation.tsx
@@ -3,29 +3,42 @@ import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { confirmEmail } from '../api';
 import '../styles/Confirmation.css';
+import { AiOutlineCheckCircle, AiOutlineCloseCircle } from 'react-icons/ai';
 
 const Confirmation: React.FC = () => {
     const location = useLocation();
     const [message, setMessage] = useState<string>('Обработка...');
+    const [status, setStatus] = useState<'success' | 'error' | 'processing'>('processing');
+
     const searchParams = new URLSearchParams(location.search);
     const code = searchParams.get('code');
 
     useEffect(() => {
         if (code) {
             confirmEmail(code)
-                .then(() => setMessage('Почта успешно подтверждена!'))
-                .catch(() => setMessage('Ошибка подтверждения.'));
+                .then(() => {
+                    setMessage('Почта успешно подтверждена!');
+                    setStatus('success');
+                })
+                .catch(() => {
+                    setMessage('Ошибка подтверждения.');
+                    setStatus('error');
+                });
         } else {
             setMessage('Код подтверждения не найден.');
+            setStatus('error');
         }
     }, [code]);
 
     return (
         <div className="confirmation-container">
+            {status === 'success' && <AiOutlineCheckCircle size={50} color="#4caf50" />}
+            {status === 'error' && <AiOutlineCloseCircle size={50} color="#f44336" />}
             <h2>Подтверждение почты</h2>
             <p>{message}</p>
         </div>
     );
 };
+
 
 export default Confirmation;
--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
+  base: mode === "production" ? "/api/" : "/", // Установка базового пути для продакшена
   server: {
     port: 5173,
     open: true,
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "https://warehouse-qr-app-8adwv.ondigitalocean.app", // Прокси для локальной разработки
         changeOrigin: true,
         secure: false,
       },
     },
   },
-})
+}))
