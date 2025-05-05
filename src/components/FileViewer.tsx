861cae058d011109c8afed05ffc04bf7f3cd7b31
Oleksandr0130 <olek1995sta@gmail.com>
2025-05-05 19:06:06 +0200

    new

--- src/components/FileViewer.tsx
+++ src/components/FileViewer.tsx
@@ -1,78 +1,3 @@
-// import React, { useEffect, useState } from 'react';
-// import '../styles/FileViever.css';
-// import api from '../api';
-//
-// const FileViewer: React.FC = () => {
-//     const [qrFiles, setQrFiles] = useState<string[]>([]);
-//     const [reservationFiles, setReservationFiles] = useState<string[]>([]);
-//
-//     const baseURL = 'http://localhost:8080/api'; // Указывается базовый URL
-//
-//     useEffect(() => {
-//         const fetchQrFiles = async () => {
-//             try {
-//                 // Получаем файлы QR-кодов с backend
-//                 const response = await api.get('/folders/qrcodes');
-//                 setQrFiles(
-//                     response.data.map((file: string) => `${baseURL}${file}`)
-//                 );
-//             } catch (error) {
-//                 console.error('Ошибка при загрузке QR-кодов:', error);
-//             }
-//         };
-//
-//         const fetchReservationFiles = async () => {
-//             try {
-//                 // Получаем резервные файлы с backend
-//                 const response = await api.get('/folders/reservation');
-//                 setReservationFiles(
-//                     response.data.map(
-//                         (file: string) => `${baseURL}${file}`
-//                     )
-//                 );
-//             } catch (error) {
-//                 console.error('Ошибка при загрузке резервных файлов:', error);
-//             }
-//         };
-//
-//         fetchQrFiles();
-//         fetchReservationFiles();
-//     }, []);
-//
-//     const handleLinkClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
-//         event.preventDefault();
-//         window.open(event.currentTarget.href, '_blank');
-//     };
-//
-//     return (
-//         <div className="file-viewer-container">
-//             <h1 className="file-viewer-title">Lager QR</h1>
-//             <ul className="file-list">
-//                 {qrFiles.map((file) => (
-//                     <li className="file-item" key={file}>
-//                         <a href={file} onClick={handleLinkClick} className="file-link">
-//                             {file.split('/').pop()}
-//                         </a>
-//                     </li>
-//                 ))}
-//             </ul>
-//
-//             <h1 className="file-viewer-title">Reserved QR</h1>
-//             <ul className="file-list">
-//                 {reservationFiles.map((file) => (
-//                     <li className="file-item" key={file}>
-//                         <a href={file} onClick={handleLinkClick} className="file-link">
-//                             {file.split('/').pop()}
-//                         </a>
-//                     </li>
-//                 ))}
-//             </ul>
-//         </div>
-//     );
-// };
-//
-// export default FileViewer;
-
 import React, { useEffect, useState } from 'react';
 import '../styles/FileViever.css';
 import api from '../api';
@@ -80,32 +5,33 @@ import api from '../api';
 const FileViewer: React.FC = () => {
     const [qrFiles, setQrFiles] = useState<string[]>([]);
     const [reservationFiles, setReservationFiles] = useState<string[]>([]);
-    const [error, setError] = useState<string | null>(null);
 
-    const baseURL = 'https://warehouse-qr-app-8adwv.ondigitalocean.app'; // Правильный базовый URL
+    const baseURL = 'http://localhost:8080/api'; // Указывается базовый URL
 
     useEffect(() => {
         const fetchQrFiles = async () => {
             try {
+                // Получаем файлы QR-кодов с backend
                 const response = await api.get('/folders/qrcodes');
                 setQrFiles(
                     response.data.map((file: string) => `${baseURL}${file}`)
                 );
             } catch (error) {
                 console.error('Ошибка при загрузке QR-кодов:', error);
-                setError('Не удалось загрузить QR-коды. Попробуйте позже.');
             }
         };
 
         const fetchReservationFiles = async () => {
             try {
+                // Получаем резервные файлы с backend
                 const response = await api.get('/folders/reservation');
                 setReservationFiles(
-                    response.data.map((file: string) => `${baseURL}${file}`)
+                    response.data.map(
+                        (file: string) => `${baseURL}${file}`
+                    )
                 );
             } catch (error) {
                 console.error('Ошибка при загрузке резервных файлов:', error);
-                setError('Не удалось загрузить резервные файлы. Попробуйте позже.');
             }
         };
 
@@ -113,54 +39,36 @@ const FileViewer: React.FC = () => {
         fetchReservationFiles();
     }, []);
 
-    const handleLinkClick = (file: string) => {
-        window.open(file, '_blank');
+    const handleLinkClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
+        event.preventDefault();
+        window.open(event.currentTarget.href, '_blank');
     };
 
     return (
         <div className="file-viewer-container">
             <h1 className="file-viewer-title">Lager QR</h1>
-            {error && <div className="error-message">{error}</div>}
             <ul className="file-list">
-                {qrFiles.map((file) => {
-                    const fileName = file.split('/').pop() || '';
-                    return (
-                        <li className="file-item" key={file}>
-                <span
-                    onClick={() => handleLinkClick(file)}
-                    className="file-link"
-                >
-                    {decodeURIComponent(fileName.replace('.png', ''))}
-                </span>
-                        </li>
-                    );
-                })}
+                {qrFiles.map((file) => (
+                    <li className="file-item" key={file}>
+                        <a href={file} onClick={handleLinkClick} className="file-link">
+                            {file.split('/').pop()}
+                        </a>
+                    </li>
+                ))}
             </ul>
-
-
-
 
             <h1 className="file-viewer-title">Reserved QR</h1>
             <ul className="file-list">
-                {reservationFiles.map((file) => {
-                    const fileName = file.split('/').pop() || '';
-                    return (
-                        <li className="file-item" key={file}>
-                <span
-                    onClick={() => handleLinkClick(file)}
-                    className="file-link"
-                >
-                    {decodeURIComponent(fileName.replace('.png', ''))}
-                </span>
-                        </li>
-                    );
-                })}
+                {reservationFiles.map((file) => (
+                    <li className="file-item" key={file}>
+                        <a href={file} onClick={handleLinkClick} className="file-link">
+                            {file.split('/').pop()}
+                        </a>
+                    </li>
+                ))}
             </ul>
-
-
         </div>
     );
 };
 
 export default FileViewer;
-
