63f9b4306a7c459c0c48c42c1f90ad63b72a1464
Oleksandr0130 <olek1995sta@gmail.com>
2025-06-09 18:50:10 +0200

    new

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === "production" ? "/api/" : "/", // Установка базового пути для продакшена
+  base: mode === "production" ? "/assets/" : "/", // Изменяем базовый путь для продакшена
   server: {
     port: 5173,
     open: true,
     proxy: {
       "/api": {
-        target: "https://warehouse-qr-app-8adwv.ondigitalocean.app/api/", // Прокси для локальной разработки
+        target: "https://warehouse-qr-app-8adwv.ondigitalocean.app/api/", // Прокси для API
         changeOrigin: true,
         secure: false,
       },
     },
   },
-}))
+}));
+
