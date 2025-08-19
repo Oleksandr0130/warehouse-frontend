c97b2f6c61dbf1e5303257929f3967e24203a689
Oleksandr0130 <olek1995sta@gmail.com>
2025-08-19 18:10:56 +0200

    new

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -28,7 +28,6 @@ const Login: React.FC<LoginProps> = ({ onSuccess }) => {
             });
             localStorage.setItem('token', response.data.token);
             onSuccess?.();
-            toast.success('Successful Login!');
             navigate('/app', { replace: true });
         } catch (err) {
             if (err instanceof AxiosError && err.response) {
