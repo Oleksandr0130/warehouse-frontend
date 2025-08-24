f31e05691bf3c933796b775a04614d9a2240c384
Oleksandr0130 <olek1995sta@gmail.com>
2025-08-24 18:15:15 +0200

    new

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import {ToastContainer} from "react-toastify";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-      <ToastContainer position="top-right" autoClose={10000} hideProgressBar={false} />
-
   </StrictMode>,
 )
